a0ecdac62df7932062ad27ad53a22d99fbd8905f
koronagaba <killleon007@gmail.com>
2022-10-24 08:05:00 +0200

    Created and used reducers action that draws the question

--- src/components/question/Question.tsx
+++ src/components/question/Question.tsx
@@ -1,25 +1,36 @@
-import React from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
-import { data } from "../../data/data";
-import { nextQuestion } from "../../features/questionsSlice";
+
+import { drawQuestion, nextQuestion } from "../../features/questionsSlice";
 
 const Question = () => {
   const dispatch = useDispatch();
-  const questionNumber = useSelector((state: RootState) => state.questions.questionNumber)
+
+  const { questionNumber, randomQuestion } = useSelector(
+    (state: RootState) => state.questions
+  );
+
+  useEffect(() => {
+    dispatch(drawQuestion());
+  }, []);
 
   const onNextQuest = () => {
     dispatch(nextQuestion());
+    dispatch(drawQuestion());
   };
+
 
   return (
     <div>
       <h1>Pyramid</h1>
       <div>
         <div>
-          <h1>{questionNumber}. {data[0].question}</h1>
+          <h1>
+            {questionNumber}. {randomQuestion?.question}
+          </h1>
           <button onClick={onNextQuest}>Next quest</button>
-          {data[0].answers.map((answer) => (
+          {randomQuestion?.answers.map((answer) => (
             <div>
               <button className="btn">{answer.answer}</button>
             </div>
--- src/features/questionsSlice.ts
+++ src/features/questionsSlice.ts
@@ -1,20 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { data } from "../data/data";
+import { SingleData } from "../types/types";
 
-
-const initialState = {
-    questionNumber: 1
+interface QuestionState {
+  questionNumber: number;
+  data: SingleData[];
+  randomQuestion: SingleData | null
 }
 
+const initialState: QuestionState = {
+  questionNumber: 1,
+  data,
+  randomQuestion: null
+};
 
 const questionsSlice = createSlice({
-    name: 'question',
-    initialState,
-    reducers: {
+  name: "question",
+  initialState,
+  reducers: {
     nextQuestion(state) {
-      state.questionNumber = state.questionNumber +1
-    }
-  }  
-})
+      state.questionNumber = state.questionNumber + 1;
+    },
+    drawQuestion(state) {
+      if (state.questionNumber <= 3) {
 
-export const {nextQuestion} = questionsSlice.actions
-export default questionsSlice.reducer
+      const random = Math.floor(Math.random() * state.data.length )
+      state.randomQuestion = state.data[random]
+      state.data.splice(random, 1)
+  
+
+    }},
+  },
+});
+
+export const { nextQuestion, drawQuestion } = questionsSlice.actions;
+export default questionsSlice.reducer;
+
