19760f1b8e892cb2e74cea2be0ca573606ffef76
Koronagaba <killleon007@gmail.com>
2022-11-25 20:45:11 +0100

    Created and Used responsiveSlice

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import {useEffect, useState} from "react";
 import Aside from "../components/aside/Aside/Aside";
 import Main from "../components/main/Main";
+import { setIsMobileFalse, setIsMobileTrue } from "../features/responsiveSlice";
+import { useAppDispatch } from "../hooks/hooks";
 import "./App.css";
 
 function App() {
+const [isMobile, setIsMobile] = useState(false)
+const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    if (window.innerWidth < 900) {
+dispatch(setIsMobileTrue())
+    } else dispatch(setIsMobileFalse())
+    window.addEventListener("resize", () => {
+      if (window.innerWidth < 900) {
+        dispatch(setIsMobileTrue())
+      } else dispatch(setIsMobileFalse())
+    });
+  }, [isMobile]);
+
+  
   return (
     <div className="wrapper">
-      <div className="container center row">
+      <div className="app_container">
           <Main />
           <Aside />
       </div>
---
+++ src/features/responsiveSlice.ts
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface InitialState {
+  isMobile: boolean;
+}
+
+const initialState: InitialState = {
+  isMobile: false,
+};
+
+const responsiveSlice = createSlice({
+  name: "responsive",
+  initialState,
+  reducers: {
+    setIsMobileFalse(state) {
+      state.isMobile = false;
+    },
+    setIsMobileTrue(state) {
+      state.isMobile = true;
+    },
+  },
+});
+
+export const { setIsMobileFalse, setIsMobileTrue } = responsiveSlice.actions;
+export default responsiveSlice.reducer;
