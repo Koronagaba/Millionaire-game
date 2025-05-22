65319e04e3a0b635c1cd174321aacf87fce971dd
Koronagaba <killleon007@gmail.com>
2025-05-22 21:36:57 +0200

    feat: reset value / styling

--- frontend/src/components/main/startView/ChatAI.tsx
+++ frontend/src/components/main/startView/ChatAI.tsx
@@ -4,24 +4,29 @@ import { generateAIResponse } from "../../../api/aiResponse";
 const ChatAI = () => {
   const [aiRequest, setAiRequest] = useState("");
   const [aiResponse, setAiResponse] = useState();
+  const [inputValue, setInputValue] = useState("");
 
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    if (!inputValue) return;
+    setAiRequest(inputValue);
+    setInputValue("");
     const response = await generateAIResponse(aiRequest);
-    console.log(response.message);
-
     setAiResponse(response.message);
   };
 
   return (
     <div className="chatBox">
-      <form onSubmit={onSubmit} className="chatForm">
+      <form
+        onSubmit={onSubmit}
+        className={`chatForm ${aiResponse ? "expanded" : "collapsed"}`}
+      >
         <input
           type="text"
           placeholder="Practice before the game..."
-          value={aiRequest}
-          onChange={(event) => setAiRequest(event.target.value)}
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
           className="chatInput"
         />
         {aiResponse && <p className="aiResponse">{aiResponse}</p>}
